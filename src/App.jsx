748e08e3f5089e2ddf735067e852f219d8321325
Phonz <119698504+iPhonz@users.noreply.github.com>
2025-02-28 13:16:38 -0800

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VerticalSPILLMarket from './components/VerticalSPILLMarket';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VerticalSPILLMarket />
+    </div>
+  );
+}
+
+export default App;
